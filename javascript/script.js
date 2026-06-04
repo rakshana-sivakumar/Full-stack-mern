8f48528aa93b32a98a216f16d668ddd995f5b91e
rakshana <rakshana.s0507@gmail.com>
2026-06-04 15:40:26 +0530

    js variables,data types,operators done

---
+++ javascript/script.js
@@ -0,0 +1,89 @@
+// var a=10; //Reintialize & Redeclare
+// let b=20; //Reintialize
+// const c=30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var num=10;
+// var str="Rakshana";
+// var undefine;
+// var Null =null;
+// var boolen = true;
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof undefine);
+// console.log(typeof Null);
+
+
+
+// var bigint = 123n;
+// var symbol = Symbol('li');
+
+// console.log(typeof bigint)
+// console.log( typeof symbol)
+
+// var arr = [10,20,30,40]
+// console.log(typeof arr , arr);
+// var obj = {
+//     name:"Rakshana",
+//     dept:["AIDS","ct"]
+// }
+// console.log(typeof Obj,obj)
+
+// var x = 10;
+// var y = 2
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x % y);
+// console.log(x / y);
+
+
+// var x = true;
+// var y = false;
+
+// console.log(x&&y);
+// console.log(x||y);
+// console.log(!x);
+
+// // p = 10;
+// // q = "10";
+
+// // console.log(p>q);
+// // console.log(p<q);
+// // console.log(p>=q);
+// // console.log(p<=q);
+// // console.log(p==q);
+// // console.log(p!=q);
+// // console.log(p===q);
+// // console.log(p!==q);
+
+// p = 10;
+// q = 20;
+// p += q;//30
+// console.log(a);
+// p-=q;
+// console.log(a);
+
+// //uniary operator(--,++)
+
+// var a = 10;
+// console.log(a++);
+// console.log(++a);
+
+// var a= 10;
+// console.log(a--);
+// console.log(--a);
+
+a = 1;
+b = 1;
+c = 0;
+
+var result = a++ + --b + ++c - ++a + ++b + c++ 
++ b++ + a;
+
+console.log(result)
