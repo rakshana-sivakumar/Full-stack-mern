0d41ff7349b34f919068440b3aec76b022239639
rakshana <rakshana.s0507@gmail.com>
2026-06-05 14:17:16 +0530

    JS completed

--- javascript/script.js
+++ javascript/script.js
@@ -79,11 +79,176 @@
 // console.log(a--);
 // console.log(--a);
 
-a = 1;
-b = 1;
-c = 0;
+// a = 1;
+// b = 1;
+// c = 0;
 
-var result = a++ + --b + ++c - ++a + ++b + c++ 
-+ b++ + a;
+// var result = a++ + --b + ++c - ++a + ++b + c++ 
+// + b++ + a;
 
-console.log(result)
+// console.log(result)
+// var a = 10;
+// var b = 20;
+// if(true){
+//     console.log(" ${a + ${b} = ${a+b}")
+// }
+// .log(a);
+// var a = 10;console
+// var a = 10;
+// let b = 20;
+// if(true){
+//     console.log(a);
+//     console.log(b);
+// }
+// console.log(a);
+// console.log(b);
+
+// var a = 10;
+// if(a%2===0){
+//     console.log(' ${a} is even ');
+// }
+// else{
+//     console.log('${a} is odd ');
+// }
+
+// var mark = 30;
+// if( mark >= 90){
+//     console.log("o grade");
+// }
+// else if(mark >= 70){
+//     console.log("A grade");
+// }
+// else if(mark >= 35){
+//     console.log("pass");
+// }
+// else{
+//     console.log("Fail");
+// }
+
+// var a = 19;
+// var result = a%2===0 ? "Even" : "Odd";
+// console.log(result)
+
+// mark = 30;
+// var result = (mark>=90) ? "O grade" :
+//              (mark>=70) ? "A grade" :
+//              (mark>=35) ?  "Pass" :
+//              "Fail";
+// console.log(result);
+
+// var day =2;
+// switch(day){
+//     case 1:{
+//         console.log("Sunday");
+//         break;
+//     }
+//     case 2:{
+//         console.log("Monday");
+//         break;
+//     }
+//     case 3:{
+//         console.log("Tuesday");
+//         break;
+//     }
+//     case 4:{
+//         console.log("Wednesdday");
+//         break;
+//     }
+//     case 5:{
+//         console.log("Thursday");
+//         break;
+//     }
+//     case 6:{
+//         console.log("Friday");
+//         break;
+//     }
+//     case 7:{
+//         console.log("Saturday");
+//         break;
+//     }
+//     Default :{
+//         console.log("Invaid input");
+//     }
+// }
+// for( var i=1;i<=10;i++){
+//     console.log(i);
+// }
+// var val = 10246;
+// var count = 0;
+// while(val > 0) {
+//     count++;
+//     val = Math.floor(val/10);
+// }
+// console.log(count);
+    
+// for(let i=1;i<=10;i++){
+//     if(i%2===0){
+//         continue;
+//     }
+//     console.log(i);
+//     }
+
+// function add(a=5, b=6){
+//     console.log(a+b);
+// }
+// add();
+
+// var demo = () => {
+//     console.log(10 + 20);
+
+// }
+// demo(10,20);
+// demo();
+// demo(10);
+
+// var arr = [ 10,20,30];
+// var arr2 = [...arr,40,50];
+// console.log(arr);
+// console.log(arr2);
+
+// var [m1,m2,m3,m4,m5] =
+//                  [90,99,98,97,100]
+// console.log(m1,m2,m3,m4,m5);
+
+// var{name,mobile,dept,Email,isactive} = {
+//     name:"Rakshana",
+//     mobile:8438858776,
+//     dept:["CT","AIDS"],
+//     email:"123@gmil.com",
+//     isactive:true
+// }
+// console.log(name,mobile,dept,Email,isactive)
+
+// var arr = [10,20,30,40,50];
+// for(let index in arr){
+//     console.log(index)
+// }
+// for(let value of arr){
+//     console.log(value)
+// }
+
+// var obj = {
+
+// }
+// var obj = {
+//     name:"Rakshana",
+//     dept:["CT","AIDS"],
+//     mobile:8438858776
+// }
+// for(let key in obj){
+//     console.log(key,obj[key])
+// }
+
+// var arr = [1,2,3,4,5];
+// var result = arr.map((val)=>(val*2));
+// console.log(result)
+
+// var even = arr.filter((val)=>(val%2===0));
+// console.log(even)
+// var sum = arr.reduce((add,val)=>(add+val),0);
+// console.log(sum)
+
+var userName = {
+    name:"Rakshana",
+    dept:
+}
